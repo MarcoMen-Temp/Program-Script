6ab7d6d25b21cd4d0f7c369fef8e3b994277f474
MarcoMen-Temp <36200629+MarcoMen-Temp@users.noreply.github.com>
2018-03-30 18:16:10 +0200

    Add files via upload

--- DataSet/format.py
+++ DataSet/format.py
@@ -1,14 +1,16 @@
 """Marco Men - Exercise 5 (Format)
-Adapted from StackOverFlow: 'Formatting Output of CSV file in Python' """ 
+ """ 
 import csv
-
-def concat(*args, sep= " | "):
+#Adapted from:'https://docs.python.org/3/tutorial/controlflow.html#more-on-defining-functions(section 4.7.3-Concat(earth,mars,venus),with "|"" as separator)
+def concat(*args, sep= "| "):
       return sep.join(args)
 
 
-print(concat("Petal Length", "Petal Width", "Sepal Length","Sepal Width"))
+print(concat("Sepal Length(cm)", "Sepal Width(cm)","Petal Length(cm)","Petal Width(cm)"))
 
+#Adapted from StackOverFlow: 'Formatting Output of CSV file in Python'
 with open("iris.csv") as f:
       reader = csv.reader(f)
       for row in reader:
-            print('{:>12} | {:>11} | {:>12} | {:>11}' .format(*row))
+            print('{:>15} | {:>14} | {:>15} | {:>14}' .format(*row))
+#The numeric values' separators were to be used to help align them to the args on top

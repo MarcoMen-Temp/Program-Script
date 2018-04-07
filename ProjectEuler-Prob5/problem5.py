a446d2870bff3b9a7e175aed077ff916a1515152
MarcoMen-Temp <36200629+MarcoMen-Temp@users.noreply.github.com>
2018-04-07 12:42:00 +0200

    Update problem5.py
    This is not my code, I only added the comments. Just thought it was the best code solution for problem5-Project Euler. Do not want to take credit,but this is the type of code I would like to write someday. I decided to keep it in this repository for further reference

--- ProjectEuler-Prob5/problem5.py
+++ ProjectEuler-Prob5/problem5.py
@@ -1,3 +1,7 @@
+# https://blog.dreamshire.com/project-euler-5-solution/
+# https://code.mikeyaworski.com/python/project_euler/problem_5"""
+
+
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? */
 
